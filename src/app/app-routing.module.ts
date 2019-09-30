4f3982c94b9248655a4bbf684790a4d6e19aceb3
Ivo Nikolov <ivo.dimitrov.nikolov@abv.bg>
2019-09-30 23:02:11 +0300

    Add routing for tabs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,17 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'places', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'auth', loadChildren: './auth/auth.module#AuthPageModule' },
   { path: 'places', loadChildren: './places/places.module#PlacesPageModule' },
-  { path: 'discover', loadChildren: './places/discover/discover.module#DiscoverPageModule' },
-  { path: 'offers', loadChildren: './places/offers/offers.module#OffersPageModule' },
-  { path: 'new-offer', loadChildren: './places/offers/new-offer/new-offer.module#NewOfferPageModule' },
-  { path: 'edit-offer', loadChildren: './places/offers/edit-offer/edit-offer.module#EditOfferPageModule' },
-  { path: 'place-detail', loadChildren: './places/discover/place-detail/place-detail.module#PlaceDetailPageModule' },
-  { path: 'offer-bookings', loadChildren: './places/offers/offer-bookings/offer-bookings.module#OfferBookingsPageModule' },
   { path: 'bookings', loadChildren: './bookings/bookings.module#BookingsPageModule' },
+  // { path: 'discover', loadChildren: './places/discover/discover.module#DiscoverPageModule' },
+  // { path: 'offers', loadChildren: './places/offers/offers.module#OffersPageModule' },
+  // { path: 'new-offer', loadChildren: './places/offers/new-offer/new-offer.module#NewOfferPageModule' },
+  // { path: 'edit-offer', loadChildren: './places/offers/edit-offer/edit-offer.module#EditOfferPageModule' },
+  // { path: 'place-detail', loadChildren: './places/discover/place-detail/place-detail.module#PlaceDetailPageModule' },
+  // { path: 'offer-bookings', loadChildren: './places/offers/offer-bookings/offer-bookings.module#OfferBookingsPageModule' },
 ];
 
 @NgModule({
--- src/app/places/places.module.ts
+++ src/app/places/places.module.ts
@@ -1,25 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
+// import { FormsModule } from '@angular/forms';
+// import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
 
 import { PlacesPage } from './places.page';
+import { PlacesRoutingModule } from './places-routing.module';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: PlacesPage
-  }
-];
+
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    PlacesRoutingModule
   ],
   declarations: [PlacesPage]
 })

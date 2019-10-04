c038ab52efc4d9fe1a9212dc04ebb0839fa91fe1
Ivo Nikolov <ivo.dimitrov.nikolov@abv.bg>
2019-10-04 17:26:00 +0300

    Add back button in places-details page

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-place-detail',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlaceDetailPage implements OnInit {
 
-  constructor() { }
+  constructor(private navCtrl: NavController) { }
 
   ngOnInit() {
+  }
+
+  onBookPlace() {
+    this.navCtrl.navigateBack('/places/tabs/discover');
   }
 
 }

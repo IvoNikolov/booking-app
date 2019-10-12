5f8bcfaf1beeea9f9b9e6774b299f893b77c502b
Ivo Nikolov <ivo.dimitrov.nikolov@abv.bg>
2019-10-12 17:22:29 +0300

    Add form to edit-form

--- src/app/places/offers/edit-offer/edit-offer.page.ts
+++ src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -3,6 +3,7 @@ import { Place } from '../../places.model';
 import { ActivatedRoute } from '@angular/router';
 import { NavController } from '@ionic/angular';
 import { PlacesService } from '../../places.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-offer',
@@ -10,13 +11,15 @@ import { PlacesService } from '../../places.service';
   styleUrls: ['./edit-offer.page.scss']
 })
 export class EditOfferPage implements OnInit {
+
+  place: Place;
+  form: FormGroup;
+
   constructor(
     private route: ActivatedRoute,
     private navCtrl: NavController,
     private placeService: PlacesService
   ) {}
-
-  place: Place;
 
   ngOnInit() {
     this.route.paramMap.subscribe(paramMap => {
@@ -26,6 +29,23 @@ export class EditOfferPage implements OnInit {
       }
       const placeId = paramMap.get('placeId');
       this.place = this.placeService.getPlace(placeId);
+      this.form = new FormGroup({
+        title: new FormControl(
+          this.place.title, {updateOn: 'blur', validators: [Validators.required]}),
+        description: new FormControl(
+          this.place.description, {updateOn: 'blur', validators: [Validators.required, Validators.maxLength(180)]}),
+        price: new FormControl(this.place.price, {updateOn: 'blur', validators: [Validators.required, Validators.min(1)]}),
+        dateFrom: new FormControl(null, {updateOn: 'blur', validators: [Validators.required]}),
+        dateTo: new FormControl(null, {updateOn: 'blur', validators: [Validators.required]}),
+      });
     });
+  }
+
+  onEditOffer() {
+    if (!this.form.valid) {
+      return;
+    }
+
+    console.log('edit');
   }
 }
--- src/app/places/offers/new-offer/new-offer.page.ts
+++ src/app/places/offers/new-offer/new-offer.page.ts
@@ -22,6 +22,10 @@ export class NewOfferPage implements OnInit {
   }
 
   onCreateOffer() {
+
+    if (!this.form.valid) {
+      return;
+    }
     console.log(this.form);
   }
 

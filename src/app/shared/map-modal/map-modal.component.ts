1c37c5993b0c8f9c93b6ea87572b4eda309b689f
Ivo Nikolov <ivo.dimitrov.nikolov@abv.bg>
2019-10-18 23:31:01 +0300

    Add map (except the environment file with th googleMapsAPI)

--- src/app/shared/map-modal/map-modal.component.ts
+++ src/app/shared/map-modal/map-modal.component.ts
@@ -1,19 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, ViewChild, Renderer2 } from '@angular/core';
 import { ModalController } from '@ionic/angular';
-
+import { environment } from '../../../environments/environment';
 @Component({
   selector: 'app-map-modal',
   templateUrl: './map-modal.component.html',
   styleUrls: ['./map-modal.component.scss'],
 })
-export class MapModalComponent implements OnInit {
+export class MapModalComponent implements OnInit, AfterViewInit {
 
-  constructor(private modalCtrl: ModalController) { }
+  @ViewChild('map', { static: true }) mapElementRef: ElementRef;
+
+  constructor(private modalCtrl: ModalController, private renderer: Renderer2, ) { }
 
   ngOnInit() {}
+
+  ngAfterViewInit() {
+    this.getGoogleMaps()
+      .then(googleMaps => {
+        const mapEl = this.mapElementRef.nativeElement;
+        const map = new googleMaps.Map(mapEl, {
+          center: { lat: 40.32, lng: 44.32 },
+          zoom: 8
+        });
+
+        googleMaps.event.addListenerOnce(map, 'idle', () => {
+          this.renderer.addClass(mapEl, 'visible');
+        });
+
+        map.addListener('click', event => {
+          const selectedCoords = {
+            lat: event.latLng.lat(),
+            lng: event.latLng.lng()
+          };
+          this.modalCtrl.dismiss(selectedCoords);
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 
   onCancel() {
     this.modalCtrl.dismiss();
   }
+
+  private getGoogleMaps(): Promise<any> {
+    const win = window as any;
+    const googleModule = win.google;
+    if (googleModule && googleModule.maps) {
+      return Promise.resolve(googleModule.maps);
+    }
+    return new Promise((resolve, reject) => {
+      const script = document.createElement('script');
+      script.src =
+        'https://maps.googleapis.com/maps/api/js?key=' + environment.googleMapsAPIkey;
+      script.async = true;
+      script.defer = true;
+      document.body.appendChild(script);
+      script.onload = () => {
+        const loadedGoogleModule = win.google;
+        if (loadedGoogleModule && loadedGoogleModule.maps) {
+          resolve(loadedGoogleModule.maps);
+        } else {
+          reject('Google maps SDK not available.');
+        }
+      };
+    });
+  }
+
 
 }
--- src/app/shared/pickers/location-picker/location-picker.component.ts
+++ src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { MapModalComponent } from '../../map-modal/map-modal.component';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
+import { map, switchMap } from 'rxjs/operators';
+import { PlaceLocation } from 'src/app/places/location.model';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-location-picker',
@@ -9,7 +14,7 @@ import { MapModalComponent } from '../../map-modal/map-modal.component';
 })
 export class LocationPickerComponent implements OnInit {
 
-  constructor(private modalCtrl: ModalController) { }
+  constructor(private modalCtrl: ModalController, private http: HttpClient) { }
 
   ngOnInit() {}
 
@@ -19,8 +24,44 @@ export class LocationPickerComponent implements OnInit {
       component: MapModalComponent
     })
     .then(modalEl => {
+      modalEl.onDidDismiss().then(modalData => {
+        if (!modalData.data) {
+          return;
+        }
+
+        const pickedLocation: PlaceLocation = {
+          lat: modalData.data.lata,
+          lng: modalData.data.lng,
+          address: null,
+          staticMapImageURL: null,
+        };
+
+        this.getAddress(modalData.data.lat, modalData.data.lng).pipe(switchMap(address => {
+          pickedLocation.address = address;
+          return of(this.getMapImage(pickedLocation.lat, pickedLocation.lng, 14));
+        })
+        ).subscribe(staticMapImageURL => {
+          pickedLocation.staticMapImageURL = staticMapImageURL;
+        });
+      });
       modalEl.present();
     });
   }
 
+  private getAddress(lat: number, lng: number) {
+    return this.http.get<any>(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${environment.googleMapsAPIkey}`)
+    .pipe(map(geoData => {
+      if (!geoData || !geoData.results || geoData.results.length === 0) {
+        return null;
+      }
+
+      return geoData.results[0].formatted_address;
+    }));
+  }
+
+  private getMapImage(lat: number, lng: number, zoom: number) {
+    return `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=${zoom}&size=500x300&maptype=roadmap
+    &markers=color:red%7Clabel:Place%7C${lat},${lng}
+    &key=${environment.googleMapsAPIkey}`;
+  }
 }

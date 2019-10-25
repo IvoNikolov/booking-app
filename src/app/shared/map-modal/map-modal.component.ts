f6162e8ef9ff8f31297bf6875e3fd602c0bf4943
Ivo Nikolov <ivo.dimitrov.nikolov@abv.bg>
2019-10-25 18:35:25 +0300

    Fix showing button

--- src/app/shared/map-modal/map-modal.component.ts
+++ src/app/shared/map-modal/map-modal.component.ts
@@ -78,6 +78,7 @@ export class MapModalComponent implements OnInit, AfterViewInit, OnDestroy {
       const script = document.createElement('script');
       script.src =
         'https://maps.googleapis.com/maps/api/js?key=' + environment.googleMapsAPIkey;
+        
       script.async = true;
       script.defer = true;
       document.body.appendChild(script);

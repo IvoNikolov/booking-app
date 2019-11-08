fe14669da89cb8161ffda951341c5e129fefe618
Ivo Nikolov <ivo.dimitrov.nikolov@abv.bg>
2019-11-08 19:08:10 +0200

    Add functionality for storing token

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import { BehaviorSubject } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { User } from './user.model';
 
 interface AuthResponseData {
   kind: string;
@@ -16,35 +19,56 @@ interface AuthResponseData {
   providedIn: 'root'
 })
 export class AuthService {
-
-  private _userIsAuthentucated = false;
-  private _userId = 'ivo';
+  private _user = new BehaviorSubject<User>(null);
+  // private _token = new BehaviorSubject<string>();
 
   get userIsAuthentucated() {
-    return this._userIsAuthentucated;
+    return this._user.asObservable().pipe(map(user => {
+      if (user) {
+        return !!user.token;
+      } else {
+        return false;
+      }
+    } ));
   }
 
   get userId() {
-    return this._userId;
+    return this._user.asObservable().pipe(map(user => {
+      if (user) {
+        return user.id;
+      } else {
+        return null;
+      }
+    }));
+
   }
 
   constructor(private http: HttpClient) { }
 
   signup(email: string, password: string) {
-    return this.http.post<AuthResponseData>(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${
-      environment.firebaseAPIkey
-    }`, {
-      email,
-      password,
-      returnSecureToken: true
-    });
+    return this.http.post<AuthResponseData>(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${
+        environment.firebaseAPIkey
+      }`,
+      { email, password, returnSecureToken: true }
+    ).pipe(tap(this.setUserData.bind(this)));
   }
 
-  login() {
-    this._userIsAuthentucated = true;
+  login(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${
+        environment.firebaseAPIkey
+      }`,
+      { email, password }
+    ).pipe(tap(this.setUserData.bind(this)));
   }
 
   logout() {
-    this._userIsAuthentucated = false;
+    this._user.next(null);
+  }
+
+  private setUserData(userData: AuthResponseData) {
+    const expirationTime = new Date(new Date().getTime() + (+userData.expiresIn * 1000));
+    return this._user.next( new User(userData.localId, userData.email, userData.idToken, expirationTime));
   }
 }
--- src/app/bookings/bookings.service.ts
+++ src/app/bookings/bookings.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Bookings } from './bookings.model';
-import { BehaviorSubject } from 'rxjs';
-import { AuthService } from '../auth/auth.service';
-import { take, map, tap, delay, switchMap } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
+import { take, tap, delay, switchMap, map } from 'rxjs/operators';
+
+import { Bookings } from './bookings.model';
+import { AuthService } from '../auth/auth.service';
 
 interface BookingData {
   bookedFrom: string;
@@ -17,18 +18,15 @@ interface BookingData {
   userId: string;
 }
 
-@Injectable({
-  providedIn: 'root'
-})
-export class BookingsService {
-
+@Injectable({ providedIn: 'root' })
+export class BookingService {
   private _bookings = new BehaviorSubject<Bookings[]>([]);
 
   get bookings() {
     return this._bookings.asObservable();
   }
 
-  constructor(private authService: AuthService, private http: HttpClient) { }
+  constructor(private authService: AuthService, private http: HttpClient) {}
 
   addBooking(
     placeId: string,
@@ -38,25 +36,33 @@ export class BookingsService {
     lastName: string,
     guestNumber: number,
     dateFrom: Date,
-    dateTo: Date,
+    dateTo: Date
   ) {
     let generatedId: string;
-    const newBooking = new Bookings(
-      'p' + Math.floor(Math.random() * 110).toString(),
-      placeId,
-      this.authService.userId,
-      placeTitle,
-      placeImage,
-      firstName,
-      lastName,
-      guestNumber,
-      dateFrom,
-      dateTo
-    );
-
-    return this.http
-    .post<{name: string}>('https://ionic-booking-app-b44d7.firebaseio.com/bookings.json', { ...newBooking, id: null })
-    .pipe(
+    let newBooking: Bookings;
+    return this.authService.userId.pipe(
+      take(1),
+      switchMap(userId => {
+        if (!userId) {
+          throw new Error('No user id found!');
+        }
+        newBooking = new Bookings(
+          Math.random().toString(),
+          placeId,
+          userId,
+          placeTitle,
+          placeImage,
+          firstName,
+          lastName,
+          guestNumber,
+          dateFrom,
+          dateTo
+        );
+        return this.http.post<{ name: string }>(
+          'https://ionic-angular-course.firebaseio.com/bookings.json',
+          { ...newBooking, id: null }
+        );
+      }),
       switchMap(resData => {
         generatedId = resData.name;
         return this.bookings;
@@ -70,53 +76,55 @@ export class BookingsService {
   }
 
   cancelBooking(bookingId: string) {
-    return this.http.delete(`https://ionic-booking-app-b44d7.firebaseio.com/bookings/${bookingId}.json`)
-    .pipe(
-      switchMap(() => {
-        return this.bookings;
-      }),
-      take(1),
-      tap(bookings => {
-      this._bookings.next(bookings.filter(b => b.id !== bookingId));
-      })
-    );
-
-    return this._bookings.pipe(take(1), delay(2000), tap(bookings => {
-      this._bookings.next(bookings.filter(b => b.id !== bookingId));
-    }));
+    return this.http
+      .delete(
+        `https://ionic-angular-course.firebaseio.com/bookings/${bookingId}.json`
+      )
+      .pipe(
+        switchMap(() => {
+          return this.bookings;
+        }),
+        take(1),
+        tap(bookings => {
+          this._bookings.next(bookings.filter(b => b.id !== bookingId));
+        })
+      );
   }
 
-  fetchBooking() {
+  fetchBookings() {
     return this.http
-    .get<{[key: string]: BookingData}>(
-      `https://ionic-booking-app-b44d7.firebaseio.com/bookings.json?orderBy="userId"&equalTo="${
-        this.authService.userId
-      }"`
-    )
-    .pipe(
-      map(bookingData => {
-        const bookings = [];
-        for (const key in bookingData) {
-          if (bookingData.hasOwnProperty(key)) {
-            bookings.push(new Bookings(
-              key,
-              bookingData[key].placeId,
-              bookingData[key].userId,
-              bookingData[key].placeTitle,
-              bookingData[key].placeImage,
-              bookingData[key].firstName,
-              bookingData[key].lastName,
-              bookingData[key].guestNumber,
-              new Date(bookingData[key].bookedFrom),
-              new Date(bookingData[key].bookedTo)
-            ));
+      .get<{ [key: string]: BookingData }>(
+        `https://ionic-angular-course.firebaseio.com/bookings.json?orderBy="userId"&equalTo="${
+          this.authService.userId
+        }"`
+      )
+      .pipe(
+        map(bookingData => {
+          const bookings = [];
+          for (const key in bookingData) {
+            if (bookingData.hasOwnProperty(key)) {
+              bookings.push(
+                new Bookings(
+                  key,
+                  bookingData[key].placeId,
+                  bookingData[key].userId,
+                  bookingData[key].placeTitle,
+                  bookingData[key].placeImage,
+                  bookingData[key].firstName,
+                  bookingData[key].lastName,
+                  bookingData[key].guestNumber,
+                  new Date(bookingData[key].bookedFrom),
+                  new Date(bookingData[key].bookedTo)
+                )
+              );
+            }
           }
-        }
-        return bookings;
-      }),
-      tap(bookings => {
-        this._bookings.next(bookings);
-      })
-    );
+          return bookings;
+        }),
+        tap(bookings => {
+          this._bookings.next(bookings);
+        })
+      );
   }
 }
+
